3aa239b84fa91f6b1fa9c749eaa34672a202d600
Usman Gill <usmangill854@gmail.com>
2021-10-21 00:59:32 +0500

    multer started

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const multer = require('multer')
+
+// millleware
+// app.use(express.json())
+
+// const db = async () => {
+//     try{await }
+// }
+const upload = multer( {
+    dest : 'upload'
+} )
+app.post('/upload',upload.single('upload'),async (req,res) =>{
+    res.send()
+})
+
+
+app.listen(1000,() => {
+    console.log('server is running on port 1000')
+})
